10e8a2e8b94ffe1436e44414705cb0a82ad1c6c7
Parker Brailow <brailowparker@berkeley.edu>
2022-12-30 22:34:11 -0700

    changed some text

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -69,8 +69,8 @@ const Menu: React.FC = () => {
     <IonMenu contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
+          <IonListHeader>Parker Brailow</IonListHeader>
+          <IonNote>brailowparker@berkeley.edu</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
