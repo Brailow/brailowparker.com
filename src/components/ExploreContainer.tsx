3a30f160100e64f359ddcfd0f398e6a66134b789
Parker Brailow <brailowparker@berkeley.edu>
2023-01-05 18:23:19 -0700

    added ability to customize pages while still rendering components via router, not a whole new page

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,6 +5,46 @@ interface ContainerProps {
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  if (name === "About") {
+    return (
+      <div className="container">
+        <strong>YOU REACHED ABOUT IN IF</strong>
+        <p>Explore</p>
+      </div>
+    );
+  }
+  if (name === "Experience") {
+    return (
+      <div className="container">
+        <strong>YOU REACHED EXPERIENCE IN IF</strong>
+        <p>Explore</p>
+      </div>
+    );
+  }
+  if (name === "Portfolio") {
+    return (
+      <div className="container">
+        <strong>YOU REACHED PORTFOLIO IN IF</strong>
+        <p>Explore</p>
+      </div>
+    );
+  }
+  if (name === "Art Gallery") {
+    return (
+      <div className="container">
+        <strong>YOU REACHED ART GALLERY IN IF</strong>
+        <p>Explore</p>
+      </div>
+    );
+  }
+  if (name === "Contact") {
+    return (
+      <div className="container">
+        <strong>YOU REACHED CONTACT IN IF</strong>
+        <p>Explore</p>
+      </div>
+    );
+  }
   return (
     <div className="container">
       <strong>{name}</strong>
